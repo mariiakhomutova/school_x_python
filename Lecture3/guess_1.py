539925fefe16443a8103ffb1f78afdb34c47e6cd
mariiakhomutova <144207305+mariiakhomutova@users.noreply.github.com>
2023-09-07 19:56:34 +0300

    =

--- Lecture3/guess_1.py
+++ Lecture3/guess_1.py
@@ -1,1 +1,13 @@
-d
+# def my_personal_sum( # объявляем функцию
+#      x: int, y: int,
+# ) -> int: #какие типы данных мы ждем (int) после функции
+# answer = x + y
+# return answer
+# print(my_personal_sum(3,5))
+
+# def my_personal_sum(
+#         for num in num_list:
+#         answer += num)
+#     return answer
+
+#args kwargs  
