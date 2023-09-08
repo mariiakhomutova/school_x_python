5b4d2cbf9754252c5b02e9d66607a036348c6e69
mariiakhomutova <144207305+mariiakhomutova@users.noreply.github.com>
2023-09-08 09:25:45 +0300

    ^

--- Lecture4/modpie.py
+++ Lecture4/modpie.py
@@ -1,1 +1,5 @@
-d
+def greet(name: str) -> str:
+    return f'Hello {name}'
+
+if __name__ == '__main__':
+    print('chototo')
--- Lecture4/scope.py
+++ Lecture4/scope.py
@@ -1,6 +1,20 @@
-x = 42
+import modpie
+z = 12
+def outer():
+    print (f'y in outer before assig:{"y" in locals()}')
+    y = 12
+    print (f'y in outer after assig:{"y" in locals()}')
+    def answer():
+        asdas = 12 + y + z 
+        print (f'y in answer:{"y" in locals()}')
+        print(locals())
+        print(globals())
+        return x
+    return answer()
 
-def answer():
-    return x
-
-print(answer())
+if __name__ == '__main__':
+    x = 42
+    print(f'x' in main exists: {"y" in locals()}) # locals - все переменные которые существуют сейчас, globals - вообще все переменные
+    #outer()
+    print(y)
+ 
